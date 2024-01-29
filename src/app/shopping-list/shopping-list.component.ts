7edabb89b33c0d9a88edbe21656af01a0ee1494a
karanara <karanara@mail.uc.edu>
2024-01-29 13:44:32 -0500

    subject-observable instead of emitter

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,21 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingService } from './shopping.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrl: './shopping-list.component.css'
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit,OnDestroy{
   ingredients:Ingredient[];
+  private igChangedSubt:Subscription;
   constructor(private slService:ShoppingService){}
   ngOnInit() {
       this.ingredients=this.slService.getIngredients();
-      this.slService.ingredientsChanged.subscribe(
+      this.igChangedSubt=this.slService.ingredientsChanged.subscribe(
         (ingredients:Ingredient[])=>{
           this.ingredients=ingredients;
         }
       );
+    }
+    ngOnDestroy(): void {
+        this.igChangedSubt.unsubscribe();
     }
 }
