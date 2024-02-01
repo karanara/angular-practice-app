4f0c444e28aae74a01e97e5f9494f3becb596faf
karanara <karanara@mail.uc.edu>
2024-02-01 16:18:15 -0500

    added dynamic component named alert

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,8 @@ import { RecipeService } from './recipes/recipe.service';
 import { AuthComponent } from './auth/auth.component';
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
+import { AlertComponent } from './alert/alert.component';
+import { PlaceholderDirective } from './shared/placeholder.directive';
 
 @NgModule({
   declarations: [
@@ -34,7 +36,9 @@ import { AuthInterceptorService } from './auth/auth-interceptor.service';
     RecipeStartComponent,
     RecipeEditComponent,
     AuthComponent,
-    LoadingSpinnerComponent
+    LoadingSpinnerComponent,
+    AlertComponent,
+    PlaceholderDirective
   ],
   imports: [
     BrowserModule,
--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,17 +1,21 @@
-import { Component } from "@angular/core";
+import { Component, ComponentFactoryResolver, OnDestroy, ViewChild } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { AuthResponseData, AuthService } from "./auth.service";
-import { Observable } from "rxjs";
+import { Observable, Subscription } from "rxjs";
 import { Router } from "@angular/router";
+import { AlertComponent } from "../alert/alert.component";
+import { PlaceholderDirective } from "../shared/placeholder.directive";
 @Component({
     selector:'app-auth',
     templateUrl:'./auth.component.html'
 })
-export class AuthComponent{
+export class AuthComponent implements OnDestroy{
      isLoginMode = true;
      isLoading=false;
      error:string=null;
-     constructor(private authService:AuthService,private router:Router){}
+     private closeSub : Subscription;
+     @ViewChild(PlaceholderDirective,{static:false}) alertHost :PlaceholderDirective;
+     constructor(private authService:AuthService,private router:Router,private componentFactoryResolver : ComponentFactoryResolver){}
      onSwitchMode(){
         this.isLoginMode=!this.isLoginMode;
      }
@@ -27,7 +31,6 @@ export class AuthComponent{
             authObs=this.authService.login(email,password);
         }else{
             this.authService.signup(email,password);
-            //form.reset();
            }
         authObs.subscribe(resData=>{
             console.log(resData);
@@ -37,8 +40,33 @@ export class AuthComponent{
          errorMessage=>{
             console.log(errorMessage);
             this.error=errorMessage;
+            this.showErrorAlert(errorMessage);
             this.isLoading=false;
         }
         );
+        form.reset();
      }
+     ngOnDestroy() {
+         if(this.closeSub){
+            this.closeSub.unsubscribe();
+         }
+     }
+     onHandleError(){
+        this.error=null;
+     }
+     private showErrorAlert(message :string){
+        const alertCmpFactory = this.componentFactoryResolver.resolveComponentFactory(
+            AlertComponent
+          );
+          const hostViewContainerRef = this.alertHost.viewContainerRef;
+          hostViewContainerRef.clear();
+      
+          const componentRef = hostViewContainerRef.createComponent(alertCmpFactory);
+      
+          componentRef.instance.message = message;
+          this.closeSub = componentRef.instance.close.subscribe(() => {
+            this.closeSub.unsubscribe();
+            hostViewContainerRef.clear();
+          });
+    }
 }
